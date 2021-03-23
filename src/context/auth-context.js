7ded4ea41605b798e9237ace8146eaf6e110c1f6
kbbushman <k.bushman@yahoo.com>
2021-03-23 13:48:42 -0700

    Adds AuthContext config

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Splash from "../components/Splash";
+import { authenticate } from "../utils/api-client";
+
+const AuthContext = React.createContext(null);
 
 export function AuthProvider({ children }) {
-  return <>{children}</>;
+  const { data, isLoading } = useQuery("AuthProvider", authenticate);
+
+  if (isLoading) return <Splash />;
+
+  const authUser = data || null;
+
+  return (
+    <AuthContext.Provider value={authUser}>{children}</AuthContext.Provider>
+  );
 }
 
 export function useAuthUser() {}
