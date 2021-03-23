e2f62ce884b685ae479c51004ef366023c51c1b4
kbbushman <k.bushman@yahoo.com>
2021-03-23 13:40:05 -0700

    Adds react query config with dev tools

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -1,6 +1,25 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./styles/main.scss";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry(failureCount, error) {
+        if (error.status === 404) return false;
+        else if (failureCount < 3) return true;
+        else return false;
+      },
+    },
+  },
+});
+
 export default function AppProviders({ children }) {
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
