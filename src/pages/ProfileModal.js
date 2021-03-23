c2de5a266de3ade79bc355ae4893f3791d76a7e7
kbbushman <k.bushman@yahoo.com>
2021-03-23 16:04:46 -0700

    Adds auth user default values for profile form

--- src/pages/ProfileModal.js
+++ src/pages/ProfileModal.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Alert, Figure, Form, Modal, Row } from "react-bootstrap";
+import { useAuthUser } from "../context/auth-context";
 
 export default function ProfileModal() {
-  // const [isLoading, setLoading] = React.useState(false);
-  // const [error, setError] = React.useState(null);
-  // const [banner, setBanner] = React.useState("");
-  // const [name, setName] = React.useState("");
-  // const [bio, setBio] = React.useState("");
-  // const [location, setLocation] = React.useState("");
-  // const [website, setWebsite] = React.useState("");
-  // const [profile, setProfile] = React.useState("");
+  const authUser = useAuthUser();
+  const [isLoading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [banner, setBanner] = React.useState(authUser?.profile_banner);
+  const [name, setName] = React.useState(authUser?.name);
+  const [bio, setBio] = React.useState(authUser?.description);
+  const [location, setLocation] = React.useState(authUser?.location);
+  const url = authUser?.entities.url.urls[0]?.url;
+  const [website, setWebsite] = React.useState(url);
+  const [profile, setProfile] = React.useState(
+    authUser?.profile_image_url_https
+  );
 
   return (
     <Modal
@@ -28,9 +33,11 @@ export default function ProfileModal() {
           </small>
         </Modal.Title>
       </Modal.Header>
-      <Alert variant="danger" className="mb-0 font-weight-bold text-white">
-        error
-      </Alert>
+      {error && (
+        <Alert variant="danger" className="mb-0 font-weight-bold text-white">
+          {error}
+        </Alert>
+      )}
       <Modal.Body className="pt-1 pb-0 px-0">
         <fieldset>
           <Form noValidate>
@@ -39,10 +46,15 @@ export default function ProfileModal() {
               style={{
                 height: "200px",
                 width: "100%",
-                backgroundImage: `url()`,
+                backgroundImage: `url(${banner})`,
               }}
             >
-              <Figure.Image src="" className="w-100 h-100" />
+              {authUser?.profile_banner_url && (
+                <Figure.Image
+                  src={authUser?.profile_banner_url}
+                  className="w-100 h-100"
+                />
+              )}
               <label
                 htmlFor="cover-image"
                 className="mx-auto my-auto btn btn-outline border px-2 py-1 font-weight-bold"
@@ -63,7 +75,7 @@ export default function ProfileModal() {
                     style={{ height: "100px", width: "100px" }}
                     className="mt-n5 rounded-circle overflow-hidden bg-primary"
                   >
-                    <Figure.Image className="w-100 h-100" src="" />
+                    <Figure.Image className="w-100 h-100" src={profile} />
                   </Figure>
                   <input
                     style={{ display: "none" }}
@@ -75,22 +87,35 @@ export default function ProfileModal() {
               </Row>
               <Form.Group controlId="name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control style={{ fontSize: "1.25rem" }} type="text" />
+                <Form.Control
+                  style={{ fontSize: "1.25rem" }}
+                  type="text"
+                  value={name}
+                />
               </Form.Group>
               <Form.Group controlId="bio">
                 <Form.Label>Bio</Form.Label>
                 <Form.Control
                   as="textarea"
                   style={{ fontSize: "1.25rem", minHeight: "100px" }}
+                  value={bio}
                 />
               </Form.Group>
               <Form.Group controlId="location">
                 <Form.Label>Location</Form.Label>
-                <Form.Control style={{ fontSize: "1.25rem" }} type="text" />
+                <Form.Control
+                  style={{ fontSize: "1.25rem" }}
+                  type="text"
+                  value={location}
+                />
               </Form.Group>
               <Form.Group controlId="website">
                 <Form.Label>Website</Form.Label>
-                <Form.Control style={{ fontSize: "1.25rem" }} type="text" />
+                <Form.Control
+                  style={{ fontSize: "1.25rem" }}
+                  type="text"
+                  value={website}
+                />
               </Form.Group>
             </div>
             <Modal.Footer className="py-1">
