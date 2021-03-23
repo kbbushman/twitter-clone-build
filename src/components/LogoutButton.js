72c2cc002d428ca4811420a22f612b1cbefc4ba1
kbbushman <k.bushman@yahoo.com>
2021-03-23 15:31:06 -0700

    Adds logout on click

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,8 +1,11 @@
-import React from "react";
+import { logout } from "../utils/api-client";
 
 export default function LogoutButton() {
   return (
-    <button className="btn btn-outline-primary rounded-pill px-2 py-1 mr-2 font-weight-bold">
+    <button
+      onClick={logout}
+      className="btn btn-outline-primary rounded-pill px-2 py-1 mr-2 font-weight-bold"
+    >
       Logout
     </button>
   );
