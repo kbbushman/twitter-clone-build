1fff6d6e815678af7ed4a44b932e234d7ee119b8
kbbushman <k.bushman@yahoo.com>
2021-03-23 13:21:42 -0700

    Adds AppProviders for app context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import AppProviders from './AppProviders';
 import "./styles/main.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AppProviders>
+    <App />
+  </AppProviders>,
+  document.getElementById("root")
+);
