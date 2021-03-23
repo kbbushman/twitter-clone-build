8f80b78009971a71d6eb0bb5168df7e28eabbf4f
kbbushman <k.bushman@yahoo.com>
2021-03-23 13:57:51 -0700

    Adds authenticate api call

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const client = axios.create();
 
-export async function authenticate() {}
+export async function authenticate() {
+  return await client.get("/auth/login").then((res) => res.data.user);
+}
 
 export async function login() {}
 
