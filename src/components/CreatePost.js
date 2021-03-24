d998c029a221ab102cb6b52d93c6108971c87242
kbbushman <k.bushman@yahoo.com>
2021-03-24 16:35:53 -0700

    Adds post form state and submit handler

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,19 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import { faImage } from "@fortawesome/free-regular-svg-icons/faImage";
 import { faSmile } from "@fortawesome/free-regular-svg-icons/faSmile";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import { Media } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useAuthUser } from "../context/auth-context";
+import { isTextValid, validate } from "../utils/validate";
+import { createPost } from "../utils/api-client";
 
 export default function CreatePost() {
+  const authUser = useAuthUser();
+  const [text, setText] = React.useState("");
+  const [disabled, setDisabled] = React.useState(false);
+
+  function handleChange(event) {
+    const text = event.target.value;
+    setText(text);
+    setDisabled(!isTextValid(text));
+  }
+
+  async function handleSubmit() {
+    try {
+      if (disabled) return;
+      const content = validate(text.trim(), "html", {
+        max_length: 280,
+        identifier: "Post",
+      });
+      setDisabled(true);
+      const post = { text: content };
+      await createPost(post);
+      setDisabled(false);
+      setText("");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <div className="p-2 mt-2">
       <Media>
-        <Link className="rounded-circle" to={`/user/auth-user-screenname`}>
+        <Link className="rounded-circle" to={`/user/${authUser?.screen_name}`}>
           <img
             className="rounded-circle"
-            src=""
-            alt=""
+            src={authUser?.profile_image_url_https}
+            alt={authUser?.screen_name}
             width={50}
             height={50}
           />
@@ -24,6 +54,8 @@ export default function CreatePost() {
             style={{ maxHeight: "80vh", height: "auto" }}
             name="text"
             placeholder="What's happening?"
+            value={text}
+            onChange={handleChange}
           />
           <div className="border-top d-flex justify-content-between align-items-center pt-2">
             <div style={{ fontSize: "1.5em" }}>
@@ -38,7 +70,11 @@ export default function CreatePost() {
               </button>
             </div>
             <div className="right">
-              <button className="btn btn-primary rounded-pill px-3 py-2 font-weight-bold">
+              <button
+                disabled={disabled}
+                onClick={handleSubmit}
+                className="btn btn-primary rounded-pill px-3 py-2 font-weight-bold"
+              >
                 Post
               </button>
             </div>
