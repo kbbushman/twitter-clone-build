74aeb24bdf16d85c4329aa223e1956cceb0ff6f1
kbbushman <k.bushman@yahoo.com>
2021-03-23 23:41:22 -0700

    Adds back button with useHistory hook

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,10 +1,15 @@
+import { useHistory } from "react-router-dom";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons/faArrowLeft";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 
 export default function BackButton() {
+  const history = useHistory();
+
   return (
-    <button className="ml-2 btn btn-naked-primary rounded-circle text-primary">
+    <button
+      onClick={history.goBack}
+      className="ml-2 btn btn-naked-primary rounded-circle text-primary"
+    >
       <FontAwesomeIcon icon={faArrowLeft} size="lg" />
     </button>
   );
