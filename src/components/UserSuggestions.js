648af6e0b4788212124b320db15ead7a28d6ed71
kbbushman <k.bushman@yahoo.com>
2021-03-25 16:50:06 -0700

    Adds user suggestions

--- src/components/UserSuggestions.js
+++ src/components/UserSuggestions.js
@@ -1,6 +1,29 @@
+import { useQuery } from "react-query";
 import UsersList from "components/UsersList";
-import React from "react";
+import { getUserSuggestions } from "../utils/api-client";
 
-export default function UserSuggestions() {
-  return <UsersList />;
+export default function UserSuggestions(props) {
+  const { authUser, ...rest } = props;
+
+  const { data, isLoading, isSuccess } = useQuery(
+    "UserSuggestions",
+    getUserSuggestions
+  );
+
+  const users = data?.filter(
+    (user) => user.screen_name !== authUser?.screen_name && !user.following
+  );
+
+  if (!users?.length) {
+    return <div className="message">No user suggestions for you</div>;
+  }
+
+  return (
+    <UsersList
+      {...rest}
+      isSuccess={isSuccess}
+      isLoading={isLoading}
+      users={users}
+    />
+  );
 }
