7eb4d0592e3866800faecff55ab6b703306a243d
kbbushman <k.bushman@yahoo.com>
2021-03-24 17:37:12 -0700

    Adds post item

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -7,46 +7,60 @@ import UserLink from "components/UserLink";
 import React from "react";
 import { Figure, ListGroup, Media, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { formatCreatedAt } from "../utils/date";
 
-export default function PostItem() {
+export default function PostItem({ post, no_reply_tag }) {
   return (
     <ListGroup.Item className="px-3" action as="div">
       <Row className="d-flex px-3 pb-1 mt-n2 text-muted">
-        <PostTag />
+        <PostTag post={post} no_reply_tag={no_reply_tag} />
       </Row>
-      <Link className="stretched-link" to={`/post/post-id-str`} />
+      <Link className="stretched-link" to={`/post/${post.id_str}`} />
       <Media className="mb-n2 w-100">
-        <UserLink className="rounded-circle" to={`/user/user-screenname`}>
+        <UserLink
+          user={post.user}
+          className="rounded-circle"
+          to={`/user/${post.user.screen_name}`}
+        >
           <Figure
             className="bg-border-color rounded-circle mr-2 overflow-hidden"
             style={{ height: "50px", width: "50px" }}
           >
-            <Figure.Image src="" className="w-100 h-100" />
+            <Figure.Image
+              src={post.user.profile_image_url_https}
+              className="w-100 h-100"
+            />
           </Figure>
         </UserLink>
         <Media.Body className="w-50">
           <Row className="d-flex align-items-center">
             <UserLink
-              to={`/user/user-screenname`}
+              user={post.user}
+              to={`/user/${post.user.screen_name}`}
               className="text-dark font-weight-bold mr-1"
             >
-              Post User Name
+              {post.user.name}
             </UserLink>
-            <span className="text-muted mr-1">@Post User Screen Name</span>
+            <span className="text-muted mr-1">@{post.user.screen_name}</span>
             <pre className="m-0 text-muted">{" - "}</pre>
-            <span className="text-muted">Post Created At</span>
+            <span className="text-muted">
+              {formatCreatedAt(post.created_at)}
+            </span>
           </Row>
           <Row className="mb-n1 mt-1">
             <blockquote className="mb-1 mw-100">
-              <PostText to={`/post/post-id-str`} />
+              <PostText post={post} to={`/post/${post.id_str}`} />
             </blockquote>
           </Row>
           <Row>
-            <MultiMedia className="mt-2" />
-            <QuotedPost className="mt-2" />
+            {/* <MultiMedia post={post} className="mt-2" />
+            <QuotedPost
+              post={!no_reply_tag && post.quoted_status}
+              className="mt-2"
+            /> */}
           </Row>
           <Row className="d-flex justify-content-end align-items-center position-static">
-            <ReactionsBar />
+            <ReactionsBar post={post} />
           </Row>
         </Media.Body>
       </Media>
