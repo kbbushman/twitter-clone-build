fa265fc85503cc52058a7eb6b6f0f572c3aee690
kbbushman <k.bushman@yahoo.com>
2021-03-24 17:47:57 -0700

    Adds formatDate for basic date format

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,13 +1,16 @@
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import format from "date-fns/format";
 
 // https://date-fns.org/v2.17.0/docs/formatDistanceToNow
-export function formatCreatedAt(timeStamp) {
-  return formatDistanceToNow(new Date(timeStamp), {
+export function formatCreatedAt(timestamp) {
+  return formatDistanceToNow(new Date(timestamp), {
     addSuffix: true,
   });
 }
 
 // https://date-fns.org/v2.17.0/docs/format
-export function formatDate() {}
+export function formatDate(timestamp) {
+  return format(new Date(timestamp), "MMM do yyyy");
+}
 
 export function formatTime() {}
