bf4f2df2ae9f150dc4d50dcd5a80d746a52465a1
kbbushman <k.bushman@yahoo.com>
2021-03-25 14:28:49 -0700

    Adds post resposts

--- src/pages/PostReposts.js
+++ src/pages/PostReposts.js
@@ -1,12 +1,24 @@
+import { useParams } from "react-router";
 import Heading from "components/Heading";
 import UsersList from "components/UsersList";
-import React from "react";
+import { useQuery } from "react-query";
+import { getPostReposts } from "../utils/api-client";
 
 export default function PostReposts() {
+  const { postId } = useParams();
+  const { data: users, isLoading, isSuccess } = useQuery("PostReposts", () =>
+    getPostReposts(postId)
+  );
+
   return (
     <>
       <Heading title="Reposted by" backButton btnProfile />
-      <UsersList noPop />
+      <UsersList
+        users={users}
+        isLoading={isLoading}
+        isSuccess={isSuccess}
+        noPop
+      />
     </>
   );
 }
