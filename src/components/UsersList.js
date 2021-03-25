624ff7805d1df95e131249c4e111440022f14d69
kbbushman <k.bushman@yahoo.com>
2021-03-25 14:22:49 -0700

    Adds users list

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,12 +1,21 @@
-import React from "react";
 import { ListGroup } from "react-bootstrap";
+import Spinner from "./Spinner";
+import UserItem from "./UserItem";
 
 export default function UsersList(props) {
-  const { className } = props;
+  const { users, isSuccess, isLoading, length, className, ...rest } = props;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <ListGroup className={`border-bottom ${className}`} variant="flush">
-      <div className="message font-weight-bold">No users to show</div>
+      {isSuccess ? (
+        users
+          .slice(0, length)
+          .map((user) => <UserItem key={user._id} user={user} {...rest} />)
+      ) : (
+        <div className="message font-weight-bold">No users to show</div>
+      )}
     </ListGroup>
   );
 }
