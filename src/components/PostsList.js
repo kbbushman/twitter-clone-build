7e9fe7696ee91fc6607bef4c4350001fe3b68e07
kbbushman <k.bushman@yahoo.com>
2021-03-24 17:37:03 -0700

    Adds posts list

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
+import PostItem from "./PostItem";
+import Spinner from "./Spinner";
 
-export default function PostsList() {
+export default function PostsList(props) {
+  const { posts, isLoading, isSuccess, no_reply_tag } = props;
+
+  if (isLoading) return <Spinner />;
+
   return (
     <ListGroup variant="flush" className="border-bottom">
-      <div className="message">No posts for you right now</div>
+      {isSuccess ? (
+        posts.map((post) => (
+          <PostItem key={post._id} post={post} no_reply_tag={no_reply_tag} />
+        ))
+      ) : (
+        <div className="message">No posts for you right now</div>
+      )}
     </ListGroup>
   );
 }
