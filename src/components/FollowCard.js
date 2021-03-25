af564900cb5f3e11d502e97f1b5b3417fc5ea173
kbbushman <k.bushman@yahoo.com>
2021-03-25 16:49:21 -0700

    Adds user suggestions for auth user

--- src/components/FollowCard.js
+++ src/components/FollowCard.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import { useAuthUser } from "../context/auth-context";
+import UserSuggestions from "./UserSuggestions";
 
 export default function FollowCard(props) {
-  const { className } = props;
+  const authUser = useAuthUser();
+  const { className, ...rest } = props;
 
   return (
     <Card className={className}>
       <Card.Header>{props.title}</Card.Header>
-      <div className="message">Login to see users and their posts</div>
+      {authUser ? (
+        <UserSuggestions authUser={authUser} length={props.length} {...rest} />
+      ) : (
+        <div className="message">Login to see users and their posts</div>
+      )}
       <Card.Footer>
         <Card.Link as={Link} to="/explore/users">
           Show more
