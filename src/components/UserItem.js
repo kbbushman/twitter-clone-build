2ddfaae7d08f026e4100a04e45a3a871418550f4
kbbushman <k.bushman@yahoo.com>
2021-03-25 14:22:57 -0700

    Adds user itme

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,43 +1,48 @@
+import { Link } from "react-router-dom";
+import { Col, ListGroup, Media, Row } from "react-bootstrap";
 import FollowButton from "components/FollowButton";
 import UserLink from "components/UserLink";
-import React from "react";
-import { Col, ListGroup, Media, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { truncateText } from "../utils/truncate";
 
-export default function UserItem({ noPop, compact }) {
+export default function UserItem({ user, noPop, compact }) {
   return (
     <ListGroup.Item
       className="px-1 text-truncate"
       action
       as={noPop ? Link : UserLink}
-      to={`/user/screen-name`}
+      to={`/user/${user.screen_name}`}
     >
       <Media>
         <img
           width={50}
           height={50}
           className="rounded-circle mx-1"
-          src=""
-          alt=""
+          src={user.profile_image_url_https}
+          alt={user.screen_name}
         />
         <Media.Body>
           <Row>
             <Col className="pr-5 pr-lg-4 pr-xl-2" xs="8">
               <p className="text-dark mb-0 text-truncate text-capitalize font-weight-bold">
-                User name
+                {user.name}
               </p>
-              <p className="text-muted text-truncate mt-n1"> @Screen Name</p>
+              <p className="text-muted text-truncate mt-n1">
+                {" "}
+                @{user.screen_name}
+              </p>
             </Col>
             <Col
               className="d-flex align-items-center justify-content-end px-1"
               xs="4"
             >
-              <FollowButton />
+              <FollowButton user={user} />
             </Col>
           </Row>
           <Row>
             {!compact && (
-              <blockquote className="mb-0 mt-n2">Description</blockquote>
+              <blockquote className="mb-0 mt-n2">
+                {truncateText(user.description, 7)}
+              </blockquote>
             )}
           </Row>
         </Media.Body>
