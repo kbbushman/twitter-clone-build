dbd23d119bd7e95a435e8fe7a53daef216e63526
kbbushman <k.bushman@yahoo.com>
2021-03-25 13:57:08 -0700

    Adds post detail

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Col, Figure, Row } from "react-bootstrap";
 import Heading from "components/Heading";
 import MultiMedia from "components/MultiMedia";
 import PostsList from "components/PostsList";
@@ -6,77 +9,97 @@ import PostText from "components/PostText";
 import QuotedPost from "components/QuotedPost";
 import ReactionsBar from "components/ReactionsBar";
 import UserLink from "components/UserLink";
-import React from "react";
-import { Col, Figure, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { getPostById, getReplies } from "../utils/api-client";
+import Spinner from "../components/Spinner";
+import { formatDate, formatTime } from "../utils/date";
 
 export default function PostDetail() {
+  const { postId } = useParams();
+  const { data: post, isLoading } = useQuery("PostDetail", () =>
+    getPostById(postId)
+  );
+  const { data: replies, isSuccess } = useQuery("PostReplies", () =>
+    getReplies(postId)
+  );
+
+  if (isLoading) return <Spinner />;
+
+  if (!post) {
+    return <div className="message font-weight-bold">Post not found</div>;
+  }
+
   return (
     <>
       <Heading backButton title="Post" />
       <Col className="p-3 d-flex flex-column">
         <Row className="d-flex px-3 pb-1 mt-n2 text-muted">
-          <PostTag />
+          <PostTag post={post} />
         </Row>
         <Row>
           <Row>
             <UserLink
               className="rounded-circle"
-              to={`/user/post-user-screenname`}
+              to={`/user/${post.user.screen_name}`}
+              user={post.user}
             >
               <Figure
                 className="bg-border-color rounded-circle mr-2 overflow-hidden"
                 style={{ height: "50px", width: "50px" }}
               >
-                <Figure.Image src="" className="w-100 h-100" />
+                <Figure.Image
+                  src={post.user.profile_image_url_https}
+                  className="w-100 h-100"
+                />
               </Figure>
             </UserLink>
             <Col className="d-flex flex-column">
               <UserLink
-                to={`/user/post-user-screenname`}
                 className="text-dark font-weight-bold mr-1"
+                to={`/user/${post.user.screen_name}`}
+                user={post.user}
               >
-                Post User Name
+                {post.user.name}
               </UserLink>
-              <span className="text-muted mr-1">@Post User Screen Name</span>
+              <span className="text-muted mr-1">@{post.user.screen_name}</span>
             </Col>
           </Row>
           <Row></Row>
         </Row>
         <Row>
           <blockquote style={{ fontSize: "1.5em" }} className="my-2 mw-100">
-            <PostText expanded />
+            <PostText post={post} expanded />
           </blockquote>
         </Row>
         <Row className="mb-2">
-          <MultiMedia expanded />
-          <QuotedPost className="mt-2" />
+          <MultiMedia post={post} expanded />
+          <QuotedPost post={post.quoted_status} className="mt-2" />
         </Row>
         <Row>
           <span className="text-muted pb-2">
-            Post Created At (Time)
+            {formatTime(post.created_at)}
             {" - "}
-            Post Created At (Date)
+            {formatDate(post.created_at)}
           </span>
         </Row>
         <Row className="border-top border-bottom d-flex p-2">
           <div className="py-1 pr-3">
-            <span className="font-weight-bold mr-1">Post Favorite Count</span>
-            <Link to={`/post/post-id-str/likes`} className="text-muted">
+            <span className="font-weight-bold mr-1">{post.favorite_count}</span>
+            <Link to={`/post/${post.id_str}likes`} className="text-muted">
               Likes
             </Link>
           </div>
           <div className="py-1 pr-3">
-            <span className="font-weight-bold mr-1">Post Retweet Count</span>
-            <Link to={`/post/post-id-str/reposts`} className="text-muted">
+            <span className="font-weight-bold mr-1">{post.retweet_count}</span>
+            <Link to={`/post/${post.id_str}/reposts`} className="text-muted">
               Reposts
             </Link>
           </div>
         </Row>
         <Row className="d-flex justify-content-end align-items-center mt-2 border-bottom">
-          <ReactionsBar />
+          <ReactionsBar post={post} />
         </Row>
-        <PostsList />
+        <PostsList posts={replies} isSuccess={isSuccess} no_reply_tag />
       </Col>
     </>
   );
