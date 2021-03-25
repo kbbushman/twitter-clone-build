6b2749bac9ac8c87269090385c4d5976746b6c53
kbbushman <k.bushman@yahoo.com>
2021-03-24 17:27:00 -0700

    Adds formatCreatedAt for timestamps

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,11 @@
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 // https://date-fns.org/v2.17.0/docs/formatDistanceToNow
-export function formatCreatedAt() {}
+export function formatCreatedAt(timeStamp) {
+  return formatDistanceToNow(new Date(timeStamp), {
+    addSuffix: true,
+  });
+}
 
 // https://date-fns.org/v2.17.0/docs/format
 export function formatDate() {}
