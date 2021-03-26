4b16a246b4e81a5fed04d12e59e25b74705a8272
kbbushman <k.bushman@yahoo.com>
2021-03-25 20:28:41 -0700

    Adds trends query

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -1,17 +1,35 @@
-import React from "react";
 import { ListGroup } from "react-bootstrap";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import Spinner from "./Spinner";
+import { getTrends } from "../utils/api-client";
 
-export default function Trends() {
+export default function Trends({ length }) {
+  const { data: trends, isLoading, isSuccess } = useQuery("Trends", getTrends);
+
+  if (isLoading) return <Spinner />;
+
+  if (!trends?.length) {
+    return <div className="message">No trends right now</div>;
+  }
+
   return (
     <ListGroup variant="flush">
-      <ListGroup.Item as={Link} action to={`/search?q=trend-query`}>
-        <small className="text-muted">Trending Worldwide</small>
-        <p className="mb-1 text-dark font-weight-bold text-capitalize">
-          Trend Name
-        </p>
-        <em>Trend Volume Tweets</em>
-      </ListGroup.Item>
+      {isSuccess &&
+        trends.slice(0, length).map((trend) => (
+          <ListGroup.Item
+            key={trend.name}
+            as={Link}
+            action
+            to={`/search?q=${trend.query}`}
+          >
+            <small className="text-muted">Trending Worldwide</small>
+            <p className="mb-1 text-dark font-weight-bold text-capitalize">
+              {trend.name}
+            </p>
+            <em>{trend.tweet_volume} Tweets</em>
+          </ListGroup.Item>
+        ))}
     </ListGroup>
   );
 }
