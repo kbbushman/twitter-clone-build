9f2f912b290f885156f959d694beac1c633a80e4
kbbushman <k.bushman@yahoo.com>
2021-03-26 00:23:30 -0700

    Adds user followers

--- src/pages/UserFollowers.js
+++ src/pages/UserFollowers.js
@@ -1,12 +1,24 @@
+import { useParams } from "react-router";
+import { useQuery } from "react-query";
 import Heading from "components/Heading";
 import UserList from "components/UsersList";
-import React from "react";
+import { getUserFollowers } from "../utils/api-client";
 
 export default function UserFollowers() {
+  const { username } = useParams();
+  const { data: users, isLoading, isSuccess } = useQuery("UserFollowers", () =>
+    getUserFollowers(username)
+  );
+
   return (
     <>
       <Heading title="Followers" backButton btnProfile />
-      <UserList noPop />
+      <UserList
+        users={users}
+        isLoading={isLoading}
+        isSuccess={isSuccess}
+        noPop
+      />
     </>
   );
 }
