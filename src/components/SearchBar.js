bc940f989238216c727f91c1e12f7c91bc9ab5f5
kbbushman <k.bushman@yahoo.com>
2021-03-25 23:11:06 -0700

    Cleans up imports

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useHistory } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
