3359f23b5faabe4fe3b7a74273c47e84f16ee373
kbbushman <k.bushman@yahoo.com>
2021-03-25 23:50:13 -0700

    Adds user detail for user/auth user

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -1,3 +1,6 @@
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Col, Figure, Row } from "react-bootstrap";
 import { faCalendarAlt as faDate } from "@fortawesome/free-solid-svg-icons/faCalendarAlt";
 import { faLink } from "@fortawesome/free-solid-svg-icons/faLink";
 import { faLocationArrow as faLocation } from "@fortawesome/free-solid-svg-icons/faLocationArrow";
@@ -6,19 +9,40 @@ import FollowButton from "components/FollowButton";
 import Heading from "components/Heading";
 import PostsList from "components/PostsList";
 import WithUrls from "components/WithUrls";
-import React from "react";
-import { Col, Figure, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { getUserTimeline } from "../utils/api-client";
+import { useAuthUser } from "../context/auth-context";
+import { formatDate } from "../utils/date";
 
 export default function UserDetail() {
+  const { username } = useParams();
+  const { data, isLoading, isSuccess } = useQuery(
+    ["UserDetail", username],
+    () => getUserTimeline(username)
+  );
+  const authUser = useAuthUser();
+
+  if (isLoading) return <Spinner />;
+
+  const user = data?.user;
+  const posts = data?.posts;
+
+  if (!user) {
+    return <div className="message font-weight-bold">User not found</div>;
+  }
+
+  const isAuthUser = authUser?.screen_name === user.screen_name;
+  const expanded_url = user.entities.url.urls[0]?.expanded_url;
+  const url = user.entities.url.urls[0]?.url;
+
   return (
     <>
-      <Heading title="" backButton />
+      <Heading title={user.name} backButton />
       <Figure
         style={{
           height: "200px",
           width: "100%",
-          backgroundImage: `url(banner)`,
+          backgroundImage: `url(${user.profile_banner})`,
         }}
       />
       <div className="p-3 border-bottom">
@@ -27,9 +51,12 @@ export default function UserDetail() {
             style={{ height: "100px", width: "100px" }}
             className="mt-n5 rounded-circle overflow-hidden bg-primary"
           >
-            <Figure.Image className="w-100 h-100" src="" />
+            <Figure.Image
+              className="w-100 h-100"
+              src={user.profile_image_url_https}
+            />
           </Figure>
-          {"isAuthUser" ? (
+          {isAuthUser ? (
             <Link
               className="btn btn-outline-primary px-3 rounded-pill font-weight-bold"
               to="/settings/profile"
@@ -37,20 +64,20 @@ export default function UserDetail() {
               Edit profile
             </Link>
           ) : (
-            <FollowButton />
+            <FollowButton user={user} />
           )}
         </Row>
         <div className="flex flex-column">
           <h5 className="mb-0">
-            <b>User Name</b>
+            <b>{user.name}</b>
           </h5>
-          <div className="text-muted">@User Screen Name</div>
+          <div className="text-muted">@{user.screen_name}</div>
         </div>
         <blockquote
           style={{ maxHeight: "300px" }}
           className="my-1 overflow-y-auto"
         >
-          <WithUrls>User Description</WithUrls>
+          <WithUrls>{user.description}</WithUrls>
         </blockquote>
         <Row className="d-flex justify-content-between mt-2">
           <Col sm="6" lg="4" className="px-2 mb-1">
@@ -60,7 +87,7 @@ export default function UserDetail() {
                 icon={faLocation}
                 style={{ fontSize: "1em" }}
               />
-              <span className="ml-1">User Location</span>
+              <span className="ml-1">{user.location || "Unknown"}</span>
             </div>
           </Col>
           <Col sm="6" lg="4" className="px-2 mb-1">
@@ -70,7 +97,7 @@ export default function UserDetail() {
                 icon={faDate}
                 style={{ fontSize: "1em" }}
               />
-              <span className="ml-1">Joined User Created At</span>
+              <span className="ml-1">Joined {formatDate(user.created_at)}</span>
             </div>
           </Col>
           <Col sm="6" lg="4" className="px-2 mb-1">
@@ -80,29 +107,29 @@ export default function UserDetail() {
                 icon={faLink}
                 style={{ fontSize: "1em" }}
               />
-              <WithUrls>Url</WithUrls>
+              <WithUrls>{expanded_url || url}</WithUrls>
             </div>
           </Col>
         </Row>
         <Row className="d-flex my-2">
           <Link
-            to={`/user/user-screenname/followers`}
+            to={`/user/${user.screen_name}/followers`}
             className="text-muted mr-2"
           >
-            User Followers Count <span>Followers</span>
+            {user.followers_count} <span>Followers</span>
           </Link>
           <Link
-            to={`/user/user-screenname/friends`}
+            to={`/user/${user.screen_name}/friends`}
             className="text-muted mr-2"
           >
-            User Friends Count <span>Following</span>
+            {user.friends_count} <span>Following</span>
           </Link>
         </Row>
       </div>
       <h5 className="m-2 pb-2 border-bottom">
-        User Statuses Count <span className="text-muted">Posts</span>
+        {user.statuses_count} <span className="text-muted">Posts</span>
       </h5>
-      <PostsList />
+      <PostsList posts={posts} isSuccess={isSuccess} />
     </>
   );
 }
