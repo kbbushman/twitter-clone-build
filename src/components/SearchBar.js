8d92061d0a51ad9338af5b462d4afbe0c7ded41c
kbbushman <k.bushman@yahoo.com>
2021-03-25 22:59:47 -0700

    Adds submit handler for search form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import { Form, InputGroup } from "react-bootstrap";
 
 function SearchBar() {
+  const history = useHistory();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const searchTerm = event.target.elements.search.value;
+    const searchQuery = encodeURIComponent(searchTerm);
+    history.push(`/search?q=${searchQuery}`);
+  }
+
   return (
-    <Form className="form-inline w-100" role="search">
+    <Form onSubmit={handleSubmit} className="form-inline w-100" role="search">
       <Form.Group
         className="w-100 mb-0 rounded-pill border-0 px-3"
         style={{ backgroundColor: "rgb(233,236,239)" }}
