1226682cd7c24fe9239a35b756c26946cee8c114
kbbushman <k.bushman@yahoo.com>
2021-03-25 22:48:51 -0700

    Adds search results

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,15 +1,30 @@
+import { useHistory, Redirect } from "react-router-dom";
 import Heading from "components/Heading";
 import PostsList from "components/PostsList";
 import UsersList from "components/UsersList";
-import React from "react";
+import { useQuery } from "react-query";
+import { getSearchResults } from "../utils/api-client";
+import Spinner from "../components/Spinner";
 
 export default function SearchResults() {
+  const history = useHistory();
+  const query = new URLSearchParams(history.location.search).get("q");
+  const { data, isLoading, isSuccess } = useQuery("Search", () =>
+    getSearchResults(query)
+  );
+
+  if (!query) return <Redirect to="/explore" />;
+
+  if (isLoading) return <Spinner />;
+
   return (
     <>
       <Heading title="Search" backButton btnProfile />
-      {"hasUsers" && <UsersList />}
-      {"hasPosts" ? (
-        <PostsList />
+      {data.users?.length && (
+        <UsersList users={data.users} isSuccess={isSuccess} />
+      )}
+      {data.posts?.length ? (
+        <PostsList posts={data.posts} isSuccess={isSuccess} />
       ) : (
         <div className="message">No posts found</div>
       )}
