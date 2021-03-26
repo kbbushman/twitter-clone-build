ae4376dc4f23e222a472acb07ec908b3872e7516
kbbushman <k.bushman@yahoo.com>
2021-03-26 00:14:05 -0700

    Adds user friends

--- src/pages/UserFriends.js
+++ src/pages/UserFriends.js
@@ -1,12 +1,24 @@
+import { useParams } from "react-router";
+import { useQuery } from "react-query";
 import Heading from "components/Heading";
 import UserList from "components/UsersList";
-import React from "react";
+import { getFriends } from "../utils/api-client";
 
 export default function UserFriends() {
+  const { username } = useParams();
+  const { data: users, isLoading, isSuccess } = useQuery("UserFriends", () =>
+    getFriends(username)
+  );
+
   return (
     <>
       <Heading title="Following" backButton btnProfile />
-      <UserList noPop />
+      <UserList
+        users={users}
+        isLoading={isLoading}
+        isSuccess={isSuccess}
+        noPop
+      />
     </>
   );
 }
