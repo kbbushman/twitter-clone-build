4103645f2affcdec1d861a17f5861fee9aa5c0f5
kbbushman <k.bushman@yahoo.com>
2021-03-25 18:28:56 -0700

    Adds state for text and button variant on hover

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useAuthUser } from "../context/auth-context";
 
-export default function FollowButton() {
+export default function FollowButton({ user }) {
+  const authUser = useAuthUser();
+  const [hoverText, setHoverText] = React.useState("");
+  const [hoverVariant, setHoverVariant] = React.useState("");
+
+  function handleMouseEnter() {
+    user.following && setHoverText("Unfollow");
+    user.following && setHoverVariant("danger");
+  }
+
+  function handleMouseLeave() {
+    setHoverText("");
+    setHoverVariant("");
+  }
+
+  const text = user.following ? "Following" : "Follow";
+  const variant = user.following ? "primary" : "outline-primary";
+
+  const hideFollowButton = !user || authUser.screen_name === user.screen_name;
+
+  if (hideFollowButton) return null;
+
   return (
-    <>
-      <Button className="rounded-pill px-3 py-1 font-weight-bold">
-        <span>Follow</span>
-      </Button>
-    </>
+    <Button
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      variant={hoverVariant || variant}
+      className="rounded-pill px-3 py-1 font-weight-bold"
+    >
+      <span>{hoverText || text}</span>
+    </Button>
   );
 }
